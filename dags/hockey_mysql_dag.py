d7cbe6810483f2ce4fed0c05b03c84506197cd61
Ayan Ashkenov <ayan.ashkenov@gmail.com>
2023-04-21 22:54:18 -0400

    removed indexing when uploading to mysql

--- dags/hockey_mysql_dag.py
+++ dags/hockey_mysql_dag.py
@@ -27,7 +27,7 @@ def upload_to_mysql(json_response):
 
     json_response_norm = pd.json_normalize(json_response['response'], sep='_')
 
-    json_response_norm.to_sql(con=engine, name='hockey_matches_raw', if_exists='append')
+    json_response_norm.to_sql(con=engine, name='hockey_matches_raw', if_exists='append', index=False)
 
 default_args = {"start_date": datetime(2023, 4, 20)}
 
